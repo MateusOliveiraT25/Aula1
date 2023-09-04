af9f05a30890f81c0c1c20ec6ae2856d769d47d2
MateusOliveiraT25 <123783867+MateusOliveiraT25@users.noreply.github.com>
2023-09-04 20:18:23 -0300

    04/09

--- Interativo/lampada.js
+++ Interativo/lampada.js
@@ -1,35 +1,50 @@
-// referências aos botões "Ligar" e "Desligar"
-const turnOn = document.getElementById( 'turnOn');
-const turnOff = document.getElementById( 'turnOff');
-//referência à imagem da lâmpada no HTML.
-const lamp = document.getElementById( 'lamp');
-//verifica se a lâmpada está quebrada verificando se o atributo src da imagem contém a string "LampadaQuebrada"
-function isLampBroken(){
-    return lamp.src.indexOf ('LampadaQuebrada')> -1
-}
-// funcao queb
-function lampBroken (){
-    lamp.src ="./img/lampadaQuebrada.jpg";
-    // Referência ao elemento de sobreposição escura
-const darkOverlay = document.getElementById('darkOverlay');
+ // Referência à imagem da lâmpada no HTML.
+ const lamp = document.getElementById('lamp');
 
-function lampOn() {
-    if (!isLampBroken()) {
-        lamp.src = "./img/lampadaLigada.jpg";
-        darkOverlay.style.display = 'none'; // Ocultar a sobreposição escura
-    }
-}
+ // Referência ao botão de alternância
+ const toggleButton = document.getElementById('toggle');
+ const titulo = document.getElementById('titulo');
+ // Variável para rastrear o número de cliques
+ let clickCount = 0;
 
-function lampOff() {
-    if (!isLampBroken()) {
-        lamp.src = "./img/lampadaDesligada.jpg";
-        darkOverlay.style.display = 'block'; // Mostrar a sobreposição escura
-    }
-}
+ // Limite de cliques para quebrar a lâmpada
+ const clickLimitToBreak = 5; // Altere para o número desejado de cliques para quebrar
 
-}
-turnOn.addEventListener( 'click', lampOn);
-turnOff.addEventListener( 'click', lampOff);
-lamp.addEventListener('mouseover', lampOn);
-lamp.addEventListener('mouseleave', lampOff);
-lamp.addEventListener('dblclick',lampBroken);
+ // Verifica se a lâmpada está quebrada verificando se o atributo src da imagem contém a string "LampadaQuebrada"
+ function isLampBroken() {
+     return lamp.src.indexOf('LampadaQuebrada') > -1;
+ }
+
+ // Função para ligar/desligar a lâmpada
+ function toggleLamp() {
+     if (isLampBroken()) {
+         // Se a lâmpada estiver quebrada, não faz nada
+         return;
+     }
+
+     if (clickCount >= clickLimitToBreak) {
+         // Se o limite de cliques for atingido, quebra a lâmpada
+         lamp.src = "./img/lampadaQuebrada.jpg";
+         // Adiciona a classe dark-background para escurecer o fundo
+         document.body.classList.add('dark-background');
+         // Oculta o botão de alternância
+         toggleButton.style.display = 'none';
+         titulo.style.display = 'none';
+         return;
+     }
+
+     if (lamp.src.indexOf('lampadaLigada.jpg') > -1) {
+         // Se a lâmpada está ligada, desliga
+         lamp.src = "./img/lampadaDesligada.jpg";
+     } else {
+         // Se a lâmpada está desligada, liga
+         lamp.src = "./img/lampadaLigada.jpg";
+     }
+
+     // Atualiza o contador de cliques
+     clickCount++;
+     document.getElementById('clickCount').textContent = `Cliques: ${clickCount}`;
+ }
+
+ // Event listener para o botão de alternância
+ toggleButton.addEventListener('click', toggleLamp);
